accdde0ea691a6f5706f094147c610374d2fa8db
Student <example@example.com>
2020-02-10 12:02:39 -0800

    add calculcate sleep method and passing tests

--- __tests__/tamagotchi.test.js
+++ __tests__/tamagotchi.test.js
@@ -35,4 +35,40 @@ describe('Should create new tamagotchi', () => {
     expect(tamagotchi.play).toEqual(99);
   });
 
+  test('should return sleep level', () => {
+    jest.advanceTimersByTime(2700001);
+    expect(tamagotchi.sleep).toEqual(10);
+  });
+
+  test('should return strong warning at sleep level 10', () => {
+    jest.advanceTimersByTime(2700001);
+    expect(tamagotchi.warning).toEqual("Warning: Your pet will pass out from exhaustion.");
+  });
+  
+  test('should return sleep level', () => {
+    jest.advanceTimersByTime(2100001);
+    expect(tamagotchi.sleep).toEqual(30);
+  });
+  
+  test('should return gentle warning at sleep level 30', () => {
+    jest.advanceTimersByTime(2100001);
+    expect(tamagotchi.warning).toEqual("Warning: Your pet needs to sleep soon.");
+  });
+
+  test('should return sleep level', () => {
+    jest.advanceTimersByTime(3000001);
+    expect(tamagotchi.sleep).toEqual(0);
+  });
+
+  test('should return sleeping as true when tamagotchi passes out', () => {
+    jest.advanceTimersByTime(3000001);
+    expect(tamagotchi.sleeping).toEqual(true);
+  });
+
+  test('should return sleeping as true when tamagotchi passes out', () => {
+    jest.advanceTimersByTime(3000001);
+    expect(tamagotchi.warning).toEqual("Your tamagotchi passed out.");
+  });
+
+
 });
--- src/tamagotchi.js
+++ src/tamagotchi.js
@@ -6,11 +6,22 @@ export class Tamagotchi {
     this.food = food;
     this.play = play;
     this.sleep = sleep;
+    this.sleeping = false;
+    this.alive = true;
+    this.warning = "";
   }
 
   calculateSleep() {
     setInterval(() => {
       this.sleep--;
+      if (this.sleep === 30) {
+        this.warning = "Warning: Your pet needs to sleep soon."
+      } else if (this.sleep === 10) {
+        this.warning = "Warning: Your pet will pass out from exhaustion."
+      } else if (this.sleep === 0) {
+        this.sleeping = true;   
+        this.warning = "Your tamagotchi passed out."
+      }
     }, 30000);
   }
 
